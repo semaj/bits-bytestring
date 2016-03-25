302e813faa8365f52fc81ed62a68fb2c5068e105
oldmanmike <oldmanmike.dev@gmail.com>
2016-03-25 17:07:26 -0400

    Add more benchmarks

--- bench/BenchmarkSuite.hs
+++ bench/BenchmarkSuite.hs
@@ -1,14 +1,25 @@
 import            Criterion.Main
 import            Data.Bits
-import            Data.Bits.ByteString
+import            Data.Bits.ByteString()
 import qualified  Data.ByteString as B
+
+zero :: B.ByteString
+zero = B.replicate 64 0
 
 bs512 :: B.ByteString
 bs512 = B.replicate 64 255
 
-benchBitwiseOperator :: (Enum a, Show a, Bits b) => (a -> b) -> a -> a -> [Benchmark]
-benchBitwiseOperator f start finish = fmap (\x -> bench (show x) $ whnf f x) [start..finish]
+bs1024 :: B.ByteString
+bs1024 = B.replicate 128 255
 
-main = defaultMain [
-  bgroup "shift" $ benchBitwiseOperator (shift bs512) 0 512
+benchBitwiseOperator :: (Int -> B.ByteString) -> [Benchmark]
+benchBitwiseOperator f = fmap (\x -> bench (show x) $ whnf f x) ([128,256,512,1024] :: [Int])
+
+main :: IO ()
+main = defaultMain
+  [ bgroup "AND" [ bench "512" $ whnf ((.&.) zero) bs512 ]
+  , bgroup "OR" [ bench "512" $ whnf ((.|.) zero) bs512 ]
+  , bgroup "XOR" [ bench "512" $ whnf (xor zero) bs512 ]
+  , bgroup "shift" $ benchBitwiseOperator (shift bs1024)
+  , bgroup "rotate" $ benchBitwiseOperator (rotate bs1024)
   ]
